417558cf684fb61c8720b74cae11894a95c4d196
Tony <anthony.scott@ca.com>
2017-11-27 20:32:24 -0700

    Initial commit for Tournament Marshal

---
+++ server/src/main/java/tmarshal/tmarshalConfiguration.java
@@ -0,0 +1,13 @@
+package tmarshal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class tmarshalConfiguration {
+
+    public static void main(String[] args) {
+        SpringApplication.run(tmarshalConfiguration.class, args);
+    }
+
+}
