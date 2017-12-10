2e096fbd4ba7c1797f065695622bca38eff3aad0
tony-tuman <tony.tuman@gmail.com>
2017-12-10 13:14:57 -0700

    consolidated login experience to single lonin page with nav-bar

--- server/src/main/java/tmarshal/server/config/WebSecurityConfig.java
+++ server/src/main/java/tmarshal/server/config/WebSecurityConfig.java
@@ -15,7 +15,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/", "/home").permitAll()
+                .antMatchers( "/css/**","/login","/js/NavBar.js","/js/UserItem.js","/js/UserForm.js","css/style.css").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
@@ -25,6 +25,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .logout()
                 .permitAll();
     }
+
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
