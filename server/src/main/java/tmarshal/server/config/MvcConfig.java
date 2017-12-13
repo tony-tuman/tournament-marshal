2eea93f2a0b02bad85f45272b9ba24181004a746
tony-tuman <tony.tuman@gmail.com>
2017-12-12 20:16:35 -0700

    fixed swagger implementation

--- server/src/main/java/tmarshal/server/config/MvcConfig.java
+++ server/src/main/java/tmarshal/server/config/MvcConfig.java
@@ -1,6 +1,8 @@
 package tmarshal.server.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
@@ -14,5 +16,15 @@ public class MvcConfig extends WebMvcConfigurerAdapter {
         registry.addViewController("/login").setViewName("login");
         registry.addViewController("/forgot-username").setViewName("forgot-username");
         registry.addViewController("/forgot-password").setViewName("forgot-password");
+    }
+
+    // Enable the swagger Web API resources.
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 }
